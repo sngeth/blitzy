9a3c064977610ede685ef34e2356400e16dc2a49
Sid Ngeth <sngeth@gmail.com>
2017-08-29 16:25:40 -0400

    Initial commit

---
+++ lib/blitzy.ex
@@ -0,0 +1,9 @@
+defmodule Blitzy do
+  def run(n_workers, url) when n_workers > 0 do
+    worker_fun = fn -> Blitzy.Worker.start(url) end
+
+    1..n_workers
+      |> Enum.map(fn _ -> Task.async(worker_fun) end)
+      |> Enum.map(&Task.await(&1, :infinity))
+  end
+end
